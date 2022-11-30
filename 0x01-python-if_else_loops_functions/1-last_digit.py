ec425bc35e4001918d7957aff9a058ab620436e0
amar-repo <umarahmed072@gmail.com>
2022-11-30 21:18:18 +0100

    Assings random signed number to variable

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+digit = abs(number) & 10
+if number < 0:
+    digit = -digit
+print(f"Last digit of {number:d} is {digit:d} and is ", end="")
+elif digit == 0:
+    print("0")
+else:
+    print("less than 6 and not 0")
